c541b4bbbf027bd5e03608274033361252b5182a
Arkhitek <takehirowakita@gmail.com>
2025-11-16 21:27:42 +0900

    feat: Add Firestore indexes and rules for user profiles and score records management

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
+  // HTML内巨大インライン<script>にもクラスが現れるためHTML再帰的スキャン
   content: [
     './public/**/*.html'
   ],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [],
+  // safelistが必要になったら以下例:
+  // safelist: ['btn', 'btn-primary', 'btn-secondary', 'option-btn']
 };
